6178688a3b06f2fb979514cc5dd1939f216a1595
Md Amaan <amaanmd622@gmail.com>
2026-04-20 19:21:39 +0530

    debug fix for render

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,18 @@ from flask import Flask
 from flask_cors import CORS
 
 def create_app():
+    print("📦 Creating Flask app")
+
     app = Flask(__name__)
     CORS(app)
 
-    # register routes
-    from app.routes.property_routes import property_bp
-    app.register_blueprint(property_bp, url_prefix="/property")
+    try:
+        print("📦 Importing routes...")
+        from app.routes.property_routes import property_bp
+        app.register_blueprint(property_bp, url_prefix="/property")
+        print("✅ Routes registered")
+    except Exception as e:
+        print("❌ ERROR in routes:", e)
 
     @app.route("/")
     def home():
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
 
+print("🚀 Starting app...")
+
 app = create_app()
 
+print("✅ App created successfully")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
