58e5ea8d6510d07958e16ff725500e92e3ff4a16
Md Amaan <amaanmd622@gmail.com>
2026-04-20 22:57:43 +0530

    sync backend fixes and frontend updates

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from app import create_app
 
-print("🚀 Starting app...")
-
 app = create_app()
 
-print("✅ App created successfully")
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
